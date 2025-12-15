07bfe5d6122e9c24e6f7d086b3b0c64e89fd7097
Zeying Zhu <zeyingz@umd.edu>
2025-12-15 18:26:51 -0500

    add go.mod in patch

--- opentelemetry-app/run.sh
+++ opentelemetry-app/run.sh
@@ -1,1 +1,1 @@
-go run ./cmd/ddsketchload   --endpoint localhost:4317   --insecure   --ddsketch-accuracy 0.01   --rate 100000   --workers 4
+go run ./cmd/ddsketchload -enable-ddsketch=false -enable-histogram=false -enable-raw=true   --endpoint localhost:4317   --insecure   --ddsketch-accuracy 0.01   --rate 100000   --workers 4
