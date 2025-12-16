a3a15b12e87be3ba67ddcf6fd389438b3844a911
Zeying Zhu <zeyingz@umd.edu>
2025-12-15 19:25:53 -0500

    update

--- opentelemetry-app/run.sh
+++
@@ -1,1 +0,0 @@
-go run ./cmd/ddsketchload -enable-ddsketch=false -enable-histogram=false -enable-raw=true   --endpoint localhost:4317   --insecure   --ddsketch-accuracy 0.01   --rate 100000   --workers 4
