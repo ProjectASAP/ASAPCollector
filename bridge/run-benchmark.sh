b81e6c22f8c075e830110601a31554b50b83cf99
Leo Wang <leozwang2005@gmail.com>
2025-12-30 12:35:43 -0500

    benchmark now reports cpu and memory

--- bridge/run-benchmark.sh
+++ bridge/run-benchmark.sh
@@ -6,8 +6,8 @@ help() {
     echo "  -c 40000                          num metrics to send per second, 0 for unbounded"
     echo "  -d 30                             duration in seconds"
     echo "  -w 10                             num workers"
-    echo "  -o ./configs/otel-bench.yaml      path to otel config file"
-    echo "  -t ./configs/telegraf-bench.conf  path to telegraf config file"
+    echo "  -o configs/otel-bench.yaml      path to otel config file"
+    echo "  -t configs/telegraf-bench.conf  path to telegraf config file"
     echo
     echo "Total number of metrics sent = count * duration * workers"
 }
@@ -38,24 +38,16 @@ workers="${_workers[-1]}"
 otelPath="${_otelPath[-1]}"
 telegrafPath="${_telegrafPath[-1]}"
 
-# otel and telegraf path are relative to script location, but if user provides a path we want that to be relative to where they are
-base=$(pwd)
 # shellcheck disable=SC2164
-if [[ "${#_otelPath[@]}" -eq 1 ]]; then
-    cd "$(dirname "$0")"
-    otelPath="$(cd -- "$(dirname -- "$otelPath")"; pwd)/$(basename -- "$otelPath")"
-    cd "$base"
-else
-    otelPath="$(cd -- "$(dirname -- "$otelPath")"; pwd)/$(basename -- "$otelPath")"
+absSrc=$( (cd "$(dirname "$0")"; pwd) )
+# shellcheck disable=SC2164
+if [[ "${#_otelPath[@]}" -eq 1 ]]; then otelPath="$absSrc/$otelPath"
+else otelPath="$(cd -- "$(dirname -- "$otelPath")"; pwd)/$(basename -- "$otelPath")"
 fi
 
 # shellcheck disable=SC2164
-if [[ "${#_telegrafPath[@]}" -eq 1 ]]; then
-    cd "$(dirname "$0")"
-    telegrafPath="$(cd -- "$(dirname -- "$telegrafPath")"; pwd)/$(basename -- "$telegrafPath")"
-    cd "$base"
-else
-    telegrafPath="$(cd -- "$(dirname -- "$telegrafPath")"; pwd)/$(basename -- "$telegrafPath")"
+if [[ "${#_telegrafPath[@]}" -eq 1 ]]; then telegrafPath="$absSrc/$telegrafPath"
+else telegrafPath="$(cd -- "$(dirname -- "$telegrafPath")"; pwd)/$(basename -- "$telegrafPath")"
 fi
 
 # ============================= run otel and telegraf
@@ -68,7 +60,9 @@ rm -rf out 2> /dev/null
 mkdir out 2> /dev/null
 
 otelOut="out/otel-out"
+otelMonitorOut="out/otel-monitor"
 telegrafOut="out/telegraf-out"
+telegrafMonitorOut="out/telegraf-monitor"
 telemetrygenOut="out/telemetrygen-out"
 
 # capture both stdout and stderr into same file (this is what terminal sees; gives better sense of temporality)
@@ -103,7 +97,12 @@ echo "=================================="
 echo
 echo "Running..."
 
-start_time=$(date +"%s.%3N")
+echo "timestamp,cpu%,memory_mb" > "$otelMonitorOut"
+echo "timestamp,cpu%,memory_mb" > "$telegrafMonitorOut"
+./monitor.sh "$otelPid" >> "$otelMonitorOut" &
+./monitor.sh "$telegrafPid" >> "$telegrafMonitorOut" &
+
+startTime=$(date +"%s.%3N")
 
 # total metrics = rate * duration * workers
 (telemetrygen metrics \
@@ -115,55 +114,98 @@ start_time=$(date +"%s.%3N")
     --unique-timeseries \
 |& cat) > "$telemetrygenOut"
 
+# TODO: replace telemetrygen with the load generator here https://github.com/ProjectASAP/DataCollector/pull/25
+# (once its merged)
+
+# note that this is not 100% accurate - we do not account for the telemetrygen start up time
+# currently, telemetrygen appears to take ~10% longer than the desired duration for duration > 60s
+endTime=$(date +"%s.%3N")
+trueDuration=$(bc <<< "$endTime - $startTime")
+
 kill "$otelPid"
 kill "$telegrafPid"
 
-# TODO: telemetrygen seems to take 10% longer than given duration
-# TODO: warn if telegraf duration is less than expected; likely not enough rotation archives in conf
-
-end_time=$(date +"%s.%3N")
-diff=$(bc <<< "$end_time - $start_time")
-echo "DURATION: $diff"
+echo "Finished running. Calculating benchmarks..."
+echo
 
 # otel stats (from opentelemetry-collector-contrib-patch/processor/kllprocessor/kll_bench.sh)
 otelSent=$(grep "metrics generated" "$telemetrygenOut" | awk '{sum+=$NF} END {print sum+0}' | tr -d '"')
-if [ "$otelSent" -eq 0 ]; then otelMps=0; else otelMps=$((otelSent / duration)); fi
-
-echo
-echo "OTEL Metrics/s: $otelMps"
-echo
+if [[ "$otelSent" -eq 0 ]]; then
+    otelMps=0
+else
+    otelMps=$(bc <<< "$otelSent / $trueDuration")
+fi
 
 # telegraf stats
 py=$(command -v python || command -v python3)
 stats=$("$py" ../telegraf_benchmarks/summarize_telegraf_metrics.py --results-dir out)
-echo -e "$stats"
+echo -e "$stats" > ./out/telegraf-bench-results
 values=$(echo -e "$stats" |
     # get the sum window, total gathered, written, and dropped values
+    # also get max latency and averages
     awk -F " " \
         -v window=0 -v gathered=0 -v written=0 -v dropped=0 \
-        '/Window:/ {gsub(",", "", $0); window+=$2; gathered+=$5; written+=$7; dropped+=$9}
-        END {print window, gathered, written, dropped}' | \
+        -v lg_avg=0 -v lg_max=0 -v lw_avg=0 -v lw_max=0 \
+        -v c_g=0 -v c_w=0 \
+        '/Window:/ {gsub(",", "", $0);
+            window+=$2; gathered+=$5; written+=$7; dropped+=$9;
+            c_g=$5; c_w=$7;
+        }
+        /Latency:/ {gsub("=", " ", $0);
+            if ($7 > lg_max) lg_max=$7;
+            if ($14 > lw_max) lw_max=$14;
+            lg_avg+=(c_g * $4);
+            lw_avg+=(c_w * $11);
+        }
+        END {
+            if (gathered != 0) { lg_avg /= gathered } else lg_avg=0;
+            if (written != 0) { lw_avg /= written } else lw_avg=0;
+            print window, gathered, written, dropped, lg_avg, lg_max, lw_avg, lw_max;
+        }' | \
     tail -n 1)
 # shellcheck disable=SC2206
 values=($values)
+read -r window gathered written dropped gatherLatencyAvg gatherLatencyMax writeLatencyAvg writeLatencyMax <<< "${values[@]}"
 
-window="${values[0]}"
-gathered="${values[1]}"
-written="${values[2]}"
-dropped="${values[3]}"
-
-if (( $(bc <<< "$window == 0") )); then
-    window=1
+# -5 to account for 5s start up time, +2s as buffer
+if (( $(bc <<< "($window - 3) < $trueDuration") )); then
+    echo "WARN: Recorded duration in Telegraf benchmark logs ($window) is less than expected duration. You may need to increase rotation_max_archives in the Telegraf config."
 fi
 
-gathered_s=$(bc <<< "$gathered/$window")
-written_s=$(bc <<< "$written/$window")
+gathered_s=$(bc <<< "$gathered/$trueDuration")
+written_s=$(bc <<< "$written/$trueDuration")
+
+stat() {
+    awk -F " " \
+        -v cpu_max=0 -v cpu_avg=0 -v mem_max=0 -v mem_avg=0 -v count=0 \
+        '{  count++;
+            cpu_avg+=$2; mem_avg+=$3;
+            if ($2 > cpu_max) cpu_max=$2;
+            if ($3 > mem_max) mem_max=$3 }
+        END {
+            if (count == 0) count=1;
+            print cpu_avg / count "%:" cpu_max "%:" mem_avg / count " MB:" mem_max " MB"
+        }' \
+    < "$1"
+}
+
+otelStats=$(stat "$otelMonitorOut")
+telegrafStats=$(stat "$telegrafMonitorOut")
 
 echo "============ SUMMARY ============="
-echo "Duration:     $window"
+echo "Duration:     ${trueDuration}s"
 echo "Dropped:      $dropped"
 echo
-echo "OTEL in/out:  $otelSent ($otelMps/s)"
-echo "Telegraf in:  $gathered ($gathered_s/s)"
-echo "Telegraf out: $written ($written_s/s)"
+echo "Metric Throughput"
+echo "  OTEL in/out:  $otelSent ($otelMps/s)"
+echo "  Telegraf in:  $gathered ($gathered_s/s)"
+echo "  Telegraf out: $written ($written_s/s)"
+echo
+echo -e "Telegraf Latency:Average:Max
+  In  (gather):$gatherLatencyAvg ms:$gatherLatencyMax ms
+  Out (write):$writeLatencyAvg ms:$writeLatencyMax ms" | column -t -s':'
+echo
+echo -e "Statistics:Avg CPU:Max CPU:Avg Memory:Max Memory
+  OTEL:${otelStats}
+  Telegraf:${telegrafStats}" | column -t -s':'
 echo "=================================="
