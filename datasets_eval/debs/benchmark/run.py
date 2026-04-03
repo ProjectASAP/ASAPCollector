b2f20f754f083f6bb34f7ed7d17f0f22eeeec89e
Gnanesh <gnanesh.dometti@gmail.com>
2026-04-03 01:37:17 -0400

    minor change

--- datasets_eval/debs/benchmark/run.py
+++ datasets_eval/debs/benchmark/run.py
@@ -58,6 +58,7 @@ QUERY_CONFIG: dict[str, QueryCfg] = {
     "Q8": QueryCfg(("quantile",), "15m", "data_filtered", ("symbol",), "quantile"),
     "Q9": QueryCfg((), "5m", "data_filtered", (), "nop"),
     "Q10": QueryCfg((), "5m", "data_filtered", (), "nop"),
+    "Q11": QueryCfg((), "5m", "data_filtered", (), "nop"),
 }
 
 NOP_QUERIES = frozenset(q for q, c in QUERY_CONFIG.items() if c.sketch_family == "nop")
