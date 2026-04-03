2d5977908ece45c47a481f258090158d957dcec7
Gnanesh <gnanesh.dometti@gmail.com>
2026-04-02 21:12:09 -0400

    q6: add QUERY_CONFIG and ground truth routing

--- datasets_eval/debs/benchmark/ground_truth/run_gt.py
+++ datasets_eval/debs/benchmark/ground_truth/run_gt.py
@@ -15,8 +15,8 @@ from common import DEFAULT_DAYS
 
 from ground_truth.tasks import run_ground_truth_task
 
-ACCEPTED_GT_QUERIES: tuple[str, ...] = ("Q1", "Q2", "Q1Q2", "Q3", "Q4", "Q5")
-ALL_GT_BATCH: tuple[str, ...] = ("Q1Q2", "Q3", "Q4", "Q5")
+ACCEPTED_GT_QUERIES: tuple[str, ...] = ("Q1", "Q2", "Q1Q2", "Q3", "Q4", "Q5", "Q6")
+ALL_GT_BATCH: tuple[str, ...] = ("Q1Q2", "Q3", "Q4", "Q5", "Q6")
 
 
 def _shutdown_executor_on_failure(
